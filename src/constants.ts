174d5c81ad1f7cbf07d05b1fa071cc5bf6b72bda
Arohan <arohans@theodo.co.uk>
2023-05-30 10:21:14 +0100

    update hint

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { FailureMessage, GameConfig, ModalConfig } from "./types";
 
 export const ARUBA_FLIGHT_DATE = new Date(2023, 5, 22, 10);
 export const CORRECT_WORD = "PEPAS";
-export const FIRST_WORD_HINT = `Fun to watch as a kid, but haram to eat`;
+export const FIRST_WORD_HINT = `A childhood TV character's first name who'd be haram to eat`;
 export const SUCCESS_EXPLANATION = `(adj) An absolute banger that we'll hear a lot of in Aruba, or so I've been told`;
 export const DEFAULT_GAME_CONFIG: GameConfig = {
   guesses: ["", "", "", "", ""],
