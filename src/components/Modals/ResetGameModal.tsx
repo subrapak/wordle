e59963758d66f996b81a1c2293d7320e9b06b7cb
Arohan <arohans@theodo.co.uk>
2023-06-09 11:37:05 +0100

    remove checkbox

--- src/components/Modals/ResetGameModal.tsx
+++ src/components/Modals/ResetGameModal.tsx
@@ -17,8 +17,28 @@ export const ResetGameModal: React.FC<ResetGameModalProps> = ({
   const [isChecked, setIsChecked] = useState(false);
   return (
     <BaseModal isVisible={isVisible} handleCloseModal={handleCloseModal}>
-      <p className="m-0 mb-4 text-black text-xl font-mono text-center">{`${username}, enough is enough.`}</p>
-      <p className="text-black text-xs font-mono text-center">{`Some people have been abusing their inside knowledge a little too much. So I did a couple of things.`}</p>
+      <p className="m-0 mb-4 text-black text-xl font-mono text-center">{`${username}, today's Wordle is Special.`}</p>
+      <>
+        <Button
+          variant="contained"
+          color="info"
+          onClick={() => {
+            onClickButton();
+            handleCloseModal();
+          }}
+          className="my-2"
+        >
+          Win Game!
+        </Button>
+        <p className="text-black text-md font-mono text-center">{`Click the button to win in 1 go.`}</p>
+        <p className="text-black text-xs font-mono text-center">{`PS. There is no actual word today, don't bother guessing.`}</p>
+        <p className="text-black text-xs font-mono text-center">{`PPS. No, Mihir can't see any of this.`}</p>
+        <p className="text-black text-xs font-mono text-center">
+          {`PPPS. @Rad I swear if you tell him we'll all know it was you and I'm ratting you out`}
+          <i>{" immediately"}</i>
+        </p>
+      </>
+      {/* <p className="text-black text-xs font-mono text-center">{`Some people have been abusing their inside knowledge a little too much. So I did a couple of things.`}</p> */}
       <ol>
         <li className="text-black text-xs font-mono m-0">{`I made this game impossible to win if your name is 'Mihir'`}</li>
         <li className="text-black text-xs font-mono m-0">{`Otherwise, you can win just by clicking the button below`}</li>
@@ -38,27 +58,6 @@ export const ResetGameModal: React.FC<ResetGameModalProps> = ({
           label="Check this box to win the game (PS it's impossible otherwise)"
         />
       </div> */}
-
-      <>
-        <p className="text-black text-md font-mono text-center">{`Good Choice.`}</p>
-        <Button
-          variant="contained"
-          color="info"
-          onClick={() => {
-            onClickButton();
-            handleCloseModal();
-          }}
-          className="my-2"
-        >
-          Win Game!
-        </Button>
-        <p className="text-black text-xs font-mono text-center">{`PS. There is no actual word today, don't bother guessing.`}</p>
-        <p className="text-black text-xs font-mono text-center">{`PPS. No, Mihir can't see any of this.`}</p>
-        <p className="text-black text-xs font-mono text-center">
-          {`PPPS. @Rad I swear if you tell him we'll all know it was you and I'm ratting you out`}
-          <i>{" immediately"}</i>
-        </p>
-      </>
     </BaseModal>
   );
 };
